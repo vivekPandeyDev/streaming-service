6b1a9e1d3e0dbd6141ce1bb14891df3bb0a49d85
vivekPandeyDev <codewithvivekpandey@gmail.com>
2026-05-29 14:17:47 +0530

    changes in html file to handle query parameter

--- ngnix-token.sh
+++ ngnix-token.sh
@@ -1,15 +1,34 @@
 expires=$(($(date +%s)+3600))
-
-uri="/hls/sample/master.m3u8"
 secret="mysecret"
 
-md5=$(echo -n "${expires}${uri} ${secret}" \
+# ---------- MASTER ----------
+master_uri="/hls/sample/master.m3u8"
+
+master_md5=$(echo -n "${expires}${master_uri} ${secret}" \
 | openssl md5 -binary \
 | openssl base64 \
 | tr +/ -_ \
 | tr -d =)
 
-echo "expires=$expires"
-echo "md5=$md5"
+echo "MASTER:"
+echo "http://localhost:8084${master_uri}?md5=${master_md5}&expires=${expires}"
 
-curl "http://localhost:8084${uri}?md5=${md5}&expires=${expires}"
+curl "http://localhost:8084${master_uri}?md5=${master_md5}&expires=${expires}"
+
+echo
+echo "--------------------------------"
+echo
+
+# ---------- PLAYLIST ----------
+playlist_uri="/hls/sample/v0/playlist.m3u8"
+
+playlist_md5=$(echo -n "${expires}${playlist_uri} ${secret}" \
+| openssl md5 -binary \
+| openssl base64 \
+| tr +/ -_ \
+| tr -d =)
+
+echo "PLAYLIST:"
+echo "http://localhost:8084${playlist_uri}?md5=${playlist_md5}&expires=${expires}"
+
+curl "http://localhost:8084${playlist_uri}?md5=${playlist_md5}&expires=${expires}"
